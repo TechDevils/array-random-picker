bbd55e26a2e92df5af9e3604a426c619cd742c75
krehwell <fahrimuham001@gmail.com>
2020-11-27 02:02:50 +0800

    added shuffle array

--- index.js
+++ index.js
@@ -28,7 +28,19 @@ function selectRandomArrayItem(input, includeIndex) {
     return input[randomIndex];
 }
 
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
 module.exports = {
     selectRandomArrayItem,
-    switchDebug
+    switchDebug,
+    shuffleArray
 }
+
