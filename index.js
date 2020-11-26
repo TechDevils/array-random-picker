01d135d6f853fdefca22bd82215168eb8abc9365
krehwell <fahrimuham001@gmail.com>
2020-11-27 03:28:25 +0800

    add a better shuffling and randomizer parameter

--- index.js
+++ index.js
@@ -3,37 +3,35 @@ function switchDebug(){
     debug = !debug;
 }
 
-function randomNumberBetween(min, max) {
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    var r = Math.random()
-    return Math.floor(r * (max - min + 1) + min)
-}
+function selectRandomArrayItem(input, options) {
+    var includeIndex = false, range = 1;
 
-function selectRandomArrayItem(input, includeIndex) {
-    var inputLength = input.length - 1
-    var randomIndex = randomNumberBetween(0, inputLength)
+    if(arguments.length > 2)
+        return new Error("Error Too Many Params are Passed");
 
-    if(debug){
-        console.log(`Random index number ${randomIndex} input length ${inputLength}`)
+    if(typeof options === "number"){
+        includeIndex = false;
+        range = options;
     }
+
+    else if(typeof options === "boolean"){
+        range = 1;
+        includeIndex = options;
+    }
+
+    else
+        return new Error("Unknown Error Has Occured During selectRandomArrayItem()");
     
-    if(includeIndex){
-        return {
-            value: input[randomIndex],
-            key: randomIndex
-        }
-    }
 
-    return input[randomIndex];
+    var result = shuffleArray([...input]).splice(0,range);
+
+    return !includeIndex ? result : {key:input.indexOf(result[0]), value: result[0]}
 }
 
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
 }
@@ -43,4 +41,3 @@ module.exports = {
     switchDebug,
     shuffleArray
 }
-
