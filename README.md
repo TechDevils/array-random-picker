d32ffeeac5ffe6a07ef1d20452a59b06ae593945
Andrew Chadwick <andrew@storefeeder.com>
2020-11-27 19:15:19 +0000

    Updated Added select many with repeats updated error handling update to README for new features

--- README.md
+++ README.md
@@ -37,6 +37,91 @@ console.log(output);
 */ 
 ```
 
+Or also return a range items of the randomly selected items
+
+```javascript
+let arp = require("arrayrandompicker");
+var input = [
+                {name: "Ted", age : 34},
+                {name: "Jill", age : 49},
+                {name: "Harris", age : 19},
+                {name: "Yuri", age : 345}
+            ]
+var output = arp.selectRandomArrayItem(input, 2);
+
+console.log(output);
+/*
+    [
+        {name: "Jill", age : 49},
+        {name: "Yuri", age : 345}
+    ] 
+*/ 
+```
+
+Or also return a range items of the randomly selected items
+
+```javascript
+let arp = require("arrayrandompicker");
+var input = [
+                {name: "Ted", age : 34},
+                {name: "Jill", age : 49},
+                {name: "Harris", age : 19},
+                {name: "Yuri", age : 345}
+            ]
+var output = arp.selectManyRepeatingRandomArrayItems(input, 6);
+
+console.log(output);
+/*
+    [
+        {name: "Ted", age : 34},
+        {name: "Jill", age : 49},
+        {name: "Harris", age : 19},
+        {name: "Yuri", age : 345},
+        {name: "Yuri", age : 345},
+        {name: "Ted", age : 34}
+    ] 
+*/ 
+```
+
+## I WANT IT ALL option
+
+```javascript
+let arp = require("arrayrandompicker");
+var input = [
+                {name: "Ted", age : 34},
+                {name: "Jill", age : 49},
+                {name: "Harris", age : 19},
+                {name: "Yuri", age : 345}
+            ]
+var output = arp.selectRandomArrayItem(input, {range:5,includeIndex:true,allowRepeats:true});
+
+console.log(output);
+/*
+    [
+        {
+            key: 2,
+            value: {name: "Harris", age : 19}
+        },
+        {
+            key: 0,
+            value: {name: "Ted", age : 34}
+        },
+        {
+            key: 0,
+            value: {name: "Ted", age : 34}
+        },
+        {
+            key: 2,
+            value: {name: "Harris", age : 19}
+        },
+        {
+            key: 3,
+            value: {name: "Yuri", age : 345}
+        }
+    ] 
+*/ 
+```
+
 if you like check the rest of my projects at https://github.com/TechDevils
 and maybe checkout
 [![ko-fi](https://www.ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/M4M31JOPH)
--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 var debug = false;
-var options = {includeIndex: false, range: 1}
+var options = {includeIndex: false, range: 1, allowRepeats:false}
+
+function error(msg){
+    return new Error(`Error : ${msg}`);
+}
 
 function switchDebug(){
     debug = !debug;
 }
 
+function randomNumberBetween(min, max) {
+    min = Math.ceil(min)	    
+    max = Math.floor(max)	    
+    var r = Math.random()	    
+    return Math.floor(r * (max - min + 1) + min)
+}
+
+function selectManyRepeatingRandomArrayItems(input, range, includeIndex){
+    if(!input || typeof input !== "object"){
+        return error("Invalid input array");
+    }
+    var output = [];
+    var arrayLength = input.length - 1;
+    for (let i = 0; i < range; i++) {
+        var index = randomNumberBetween(0,arrayLength);
+        var outputValue = input[index];
+        if(includeIndex){
+            outputValue = {key:index, value:arrayValue };
+        }
+        output.push(outputValue)
+    }
+    return output;
+}
+
 function selectRandomArrayItem(input, args = undefined) {
-    var {includeIndex, range} = options;
+    if(!input || typeof input !== "object"){
+        return error("Invalid input array");
+    }
+
+    var {includeIndex, range, allowRepeats} = options;
 
     if(arguments.length > 2)
-        return new Error("Error: Too Many Params are Passed");
+    {
+        return error("Too Many Params are Passed");
+    }
 
     if(args !== undefined){
         if(Number.isInteger(args)){
@@ -21,11 +55,29 @@ function selectRandomArrayItem(input, args = undefined) {
             range = 1;
             includeIndex = args;
         }
-
+        else if(typeof args === "object"){
+            range = 1;
+            includeIndex = false;
+            allowRepeats = false;
+            if(args.includeIndex){
+                includeIndex = args.includeIndex;
+            }
+            if(args.range){
+                range = args.range;
+            }
+            if(args.allowRepeats){
+                allowRepeats = args.allowRepeats;
+            }
+        }
         else
-            return new Error("Error: The Arguments (args) Does not Accept Value Except Boolean or Int");
+        {
+            return error("The Arguments (args) Does not Accept Value Except Boolean or Int");
+        }
     }
-
+    if(allowRepeats){
+        return selectManyRepeatingRandomArrayItems(input, range, includeIndex);
+    }
+    //ToDo : performance test this for large arrays.
     var result = shuffleArray([...input]).splice(0,range);
 
     return !includeIndex ? result : {key:input.indexOf(result[0]), value: result[0]}
@@ -42,5 +94,6 @@ function shuffleArray(array) {
 module.exports = {
     selectRandomArrayItem,
     switchDebug,
-    shuffleArray
+    shuffleArray,
+    selectManyRepeatingRandomArrayItems
 }
